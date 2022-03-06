c1b14ee0d72dd93c88a0d797a6d399e0715c0039
jychoi <riansoul@gmail.com>
2022-03-06 21:30:14 +0900

    feat : Project 생성.

---
+++ github_searchUITests/github_searchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_searchUITestsLaunchTests.swift
+//  github_searchUITests
+//
+//  Created by Geondae Baek on 2022/03/06.
+//
+
+import XCTest
+
+class github_searchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
